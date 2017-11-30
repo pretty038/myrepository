ee6fd4a727a15f5dc2dcc921f7496566aad62511
gaoxiaoning <gaoxiaoning@maoyan.com>
2017-11-30 22:16:32 +0800

    ddd

---
+++ src/main/java/com/apcompany/api/service/impl/WalletServiceImp.java
@@ -0,0 +1,21 @@
+package com.apcompany.api.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.apcompany.api.service.IWalletService;
+@Service
+public class WalletServiceImp implements IWalletService {
+
+	@Override
+	public int getStudentMoney(int studentId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void transMoney(int studentId, int teacherId, int money) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
